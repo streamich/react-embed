ff6c8b4143d3019032e576d999e09b295f883f22
streamich <streamich@gmail.com>
2019-02-27 09:42:40 +0100

    fix: 🐛 export typings from module

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {ReactEmbedProps} from './ReactEmbed';
 
+export * from './ReactEmbed';
+
 const Resource = React.lazy(() => import('./ReactEmbed') as any);
 
 const Embed: React.SFC<ReactEmbedProps> = props => (
