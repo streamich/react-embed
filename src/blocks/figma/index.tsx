cf76ab0ea1fad7bbefecabd58adb6170bd38e1af
streamich <streamich@gmail.com>
2019-04-24 12:02:41 +0200

    feat: 🎸 make figma block fit outer box

--- src/blocks/figma/index.tsx
+++ src/blocks/figma/index.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react';
 import {BlockProps} from '../..';
+import {rule} from 'p4-css';
+
+const blockClass = rule({
+  d: 'block',
+  ov: 'hidden',
+  w: '100%',
+  bd: '1px solid #E5E9F2',
+  bdrad: '6px',
+  '& iframe': {
+    d: 'block',
+    ov: 'hidden',
+    bd: 0,
+    w: '100%',
+    h: '450px',
+    bdrad: '8px',
+  },
+});
 
 const Figma: React.SFC<BlockProps> = ({url}) => {
   return (
-    <iframe
-      frameBorder="no"
-      style={{border: 'none'}}
-      width="100%"
-      height="450px"
-      src={`https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(url)}`}
-      allowFullScreen
-    />
+    <div className={blockClass}>
+      <iframe
+        src={`https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(url)}`}
+        allowFullScreen
+      />
+    </div>
   );
 }
 
