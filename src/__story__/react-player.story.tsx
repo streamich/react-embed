ad201df87cabfc4b8d51f1c4ac08ffe98b68796a
streamich <streamich@gmail.com>
2019-04-24 11:20:34 +0200

    feat: 🎸 make JsFiddle fit box without margins

--- src/__story__/react-player.story.tsx
+++ src/__story__/react-player.story.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import Embed from '..';
+import {Box} from './Box';
 
 storiesOf('react-player', module)
   .add('Facebook', () => {
@@ -13,6 +14,13 @@ storiesOf('react-player', module)
       <Embed url={'https://www.twitch.tv/videos/290187802'} />
     );
   })
+  .add('Twitch Fitted', () => {
+    return (
+      <Box>
+        <Embed url={'https://www.twitch.tv/videos/290187802'} />
+      </Box>
+    );
+  })
   .add('DailyMotion', () => {
     return (
       <Embed url={'https://www.dailymotion.com/video/x75y5s2?playlist=x63vyg'} />
@@ -21,5 +29,12 @@ storiesOf('react-player', module)
   .add('Vimeo', () => {
     return (
       <Embed url={'https://vimeo.com/54763818'} />
+    );
+  })
+  .add('Vimeo Fitted', () => {
+    return (
+      <Box>
+        <Embed url={'https://vimeo.com/54763818'} />
+      </Box>
     );
   })
