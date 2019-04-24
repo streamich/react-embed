b63d31ef34d5c018a73f121444064b8fe754014b
streamich <streamich@gmail.com>
2019-04-24 11:54:54 +0200

    feat: 🎸 make gist fit outer box

--- src/__story__/gist.story.tsx
+++ src/__story__/gist.story.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import Embed from '..';
+import {Box} from './Box';
 
 storiesOf('gist', module)
   .add('Site link', () => <Embed url={'https://gist.github.com/derrickturk/156b06d3a8496cc15fcab587e7881073'} />)
@@ -11,3 +12,10 @@ storiesOf('gist', module)
       <Embed url={'https://gist.github.com/choco-bot/c588cdb67a420b21133f30f3e5645b22'} />
     </div>
   ))
+  .add('Fitted', () => {
+    return (
+      <Box>
+        <Embed url={'https://gist.github.com/derrickturk/156b06d3a8496cc15fcab587e7881073'} />
+      </Box>
+    );
+  })
